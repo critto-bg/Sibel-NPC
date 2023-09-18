6ca7fb28da255007285964d5a0b1789e85f83de1
RD <el.critto@gmail.com>
2023-09-18 13:45:42 +0700

    Initial commit

---
+++ SibelNPC/resources/dialogues/sibel.d
@@ -0,0 +1,50 @@
+BEGIN ~S!SIBEL~
+
+IF ~NumTimesTalkedTo(0)~ THEN BEGIN FirstMeeting
+  SAY ~Hello. My name is Sibel, Sibel Arcanis. May I ask your name?~
+  ++ ~Greetings. You certainly may. I am called <CHARNAME>. What brings you here?~ + SibelExpoBegin
+END
+
+IF ~~ THEN BEGIN SibelExpoBegin
+  SAY ~I was bargaining for some supplies with the good merchant over there. I am about to embark on an adventure, and this seemed like a reasonable first step.~
+  ++ ~Adventure, you say? Would you mind telling me more about it?~ + SibelFamily
+END
+
+IF ~~ THEN BEGIN SibelFamily
+  SAY ~Well, I see no reason why I shouldn't. My family has lived in the Forgotten Realms several centuries ago. There was a village built near and around a huge elm tree. They had been a community of librarians, druids and excellent leather craftstmen, before the curse happened.~
+  ++ ~The curse?~ + TheCurse
+END
+
+IF ~~ THEN BEGIN TheCurse
+  SAY ~My father believed that a curse was cast on the family by the gods themselves. No one in the family had chosen to become a priest in all those centuries. They were mages, warriors and scholars aplenty. None took the path of priesthood.~
+  = ~And so the elm tree grew dry and lifeless, the quality of the famous family leather dropped and the family's fortune has withered.~
+  ++ ~Go on.~ + TheCurse2
+END
+
+IF ~~ THEN BEGIN TheCurse2
+  SAY ~Before his death, Father urged me to find a way to undo the family curse. He had tried himself but failed.~
+  ++ ~Fascinating. Do you feel any effects of the curse upon you?~ + OfferJoin
+END
+
+IF ~~ THEN BEGIN OfferJoin
+  SAY ~Not really, no. But Father had told me that when the curse is lifted I would feel the difference. You seem like a capable <MANWOMAN>. Perhaps, we may join together and find a way to break the curse?~
+  ++ ~You are welcome to join me, Sibel. Together we shall see what could be done about this curse of yours.~ + SibelJoins
+  ++ ~I am afraid I have to decline your plea, Sibel. I wish you best of luck with your endeavour.~ + SayGoodbye
+END
+
+// TODO: add the usual BG2 reactivity here, protagonist asks Sibel about joining forces against Irenicus and so on
+IF ~~ THEN BEGIN SibelJoins
+  SAY ~Thank you! I am ready to go whenever you are. I will be glad to lend my strengths to you in your own quest, whatever it may be.~
+  IF ~~ THEN REPLY ~Let us be off then.~ DO ~SetGlobal("IaSibelJoined","LOCALS",1) JoinParty()~ EXIT
+END
+
+IF ~~ THEN BEGIN SayGoodbye
+  SAY ~Very well, <CHARNAME>. If you happen to change your mind, I'll be staying here for a while.~
+  IF ~~ THEN EXIT
+END
+
+IF ~NumTimesTalkedToGT(0)~ THEN BEGIN SubsequentMeeting
+  SAY ~Hello, <CHARNAME>. You're back! Have you reconsidered my proposal to join you?~
+  ++ ~Yes, you are welcome to join me, Sibel. Together we shall see what could be done about this curse of yours.~ + SibelJoins
+  ++ ~I am afraid I haven't, Sibel. I hope you will manage to break the curse.~ + SayGoodbye
+END
---
+++ SibelNPC/resources/dialogues/sibelp.d
@@ -0,0 +1,45 @@
+BEGIN ~S!SibelP~
+
+IF ~Global("IaSibelJoined","LOCALS",1)~ THEN BEGIN KickOut
+  SAY ~Do you want me to leave the group, <CHARNAME>?~
+  IF ~~ THEN REPLY ~No, Sibel, you should remain with us.~ DO ~JoinParty()~ EXIT
+  IF ~~ THEN REPLY ~Yes, we should go our separate ways for now.~ DO ~SetGlobal("IaSibelJoined","LOCALS",0)~ GOTO WhereToGo
+END
+
+IF ~Global("IaSibelJoined","LOCALS",0)~ THEN BEGIN ReJoin
+  SAY ~It is good to see you, <CHARNAME>! Do you want to me to travel with you again?~
+  IF ~~ THEN
+    REPLY ~Yes, Sibel, let us join forces together.~
+    DO ~SetGlobal("IaSibelJoined","LOCALS",1) JoinParty()~
+    GOTO SayWelcome
+  IF ~~ THEN REPLY ~No, we should go our separate ways for now.~ GOTO SayGoodbye
+END
+
+IF ~~ THEN BEGIN WhereToGo
+  SAY "Very well, <CHARNAME>. Do you want me to wait for you here?"
+  // TODO: area checks for this option?
+  IF ~~ THEN REPLY ~Yes, we'll come back for you later.~ GOTO SibelStaysPut
+  IF ~!AreaCheck("AR0702")~ THEN
+    REPLY ~No, you should go back to Ribald's store. I will look for you there if I need your help again.~
+    GOTO SibelGoesBack
+END
+
+IF ~~ THEN BEGIN SibelStaysPut
+  SAY "As you wish, <CHARNAME>, I will stay here for awhile. Goodbye."
+  IF ~~ THEN EXIT
+END
+
+IF ~~ THEN BEGIN SibelGoesBack
+  SAY "As you wish, <CHARNAME>. Goodbye."
+  IF ~~ THEN DO ~EscapeAreaMove("AR0702",755,335,3)~ EXIT
+END
+
+IF ~~ THEN BEGIN SayWelcome
+  SAY ~I am glad you have changed your mind, <CHARNAME>. Let us be off.~
+  IF ~~ THEN EXIT
+END
+
+IF ~~ THEN BEGIN SayGoodbye
+  SAY ~Very well, <CHARNAME>. If you happen to change your mind, I'll be staying here for awhile.~
+  IF ~~ THEN EXIT
+END
