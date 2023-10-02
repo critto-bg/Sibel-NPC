6444060d5d0419bab0ce37b7de232573e9fa6a63
RD <el.critto@gmail.com>
2023-10-02 10:57:45 +0400

    Crazy Celvan's poem about Sibel

---
+++ SibelNPC/resources/dialogues/celvan.d
@@ -0,0 +1,23 @@
+APPEND ~S!SibelJ~
+  IF ~~ THEN BEGIN SibelRespondsCelvan
+    SAY ~How do you know all those things?~
+    IF ~~ THEN EXTERN ~CELVAN~ 1
+  END
+END
+
+APPEND ~CELVAN~
+  IF ~See("S!Sibel")
+      InParty("S!Sibel")
+      IfValidForPartyDialogue("S!Sibel")
+      RandomNum(2,2)
+      Global("IaCelvanSibel","AR0300",0)~ THEN
+  BEGIN CelvanSibel
+    SAY ~There once was a girl and her father.
+He told of a curse to her and no other;
+If she and friends stay together
+She will then craft the finest leather,
+Though her enemies are yet to bother.~
+    IF ~~ THEN EXTERN ~S!SibelJ~ SibelRespondsCelvan
+  END
+END
+
