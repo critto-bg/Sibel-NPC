e046210ede42bef67183c8a608ccc520302e366c
RD <el.critto@gmail.com>
2023-10-03 20:31:08 +0400

    Introduce timer and XP award to the end of Reginald's quest

--- SibelNPC/resources/dialogues/reginald.d
+++ SibelNPC/resources/dialogues/reginald.d
@@ -40,25 +40,62 @@ IF ~~ THEN BEGIN Leave
 END
 
 IF ~~ THEN BEGIN GivenFruit
-  SAY ~Extraordinary! Thank you! Now, to compensate your efforts, I will use some of the fruit to brew potions for you. Tell me which ones you prefer?~
+  SAY ~Extraordinary! Thank you! To compensate your efforts, I will use some of the fruit to brew potions for you. Tell me which ones you prefer?~
 
   IF ~~ THEN
     REPLY ~I need potions that grant strength.~
-    DO ~GiveItemCreate("POTN06",Player1,8,0,0) GiveItemCreate("POTN07",Player1,4,0,0)~
-    GOTO Farewell
+    DO ~SetGlobal("IaPotionType","LOCALS",1)~
+    GOTO GoWait
 
   IF ~~ THEN
     REPLY ~I need potions of healing.~
-    DO ~GiveItemCreate("POTN55",Player1,9,0,0)~
-    GOTO Farewell
+    DO ~SetGlobal("IaPotionType","LOCALS",2)~
+    GOTO GoWait
 
   IF ~~ THEN
     REPLY ~I need potions of regeneration.~
+    DO ~SetGlobal("IaPotionType","LOCALS",3)~
+    GOTO GoWait
+END
+
+IF ~~ THEN BEGIN GoWait
+  SAY ~Clever choice! Now, allow me five hours to prepare the potions.~
+  IF ~~ THEN DO ~SetGlobal("IaSamaraPlot","GLOBAL",10) SetGlobalTimer("IaRegiTimer","GLOBAL",1500)~ EXIT
+END
+
+IF ~Global("IaSamaraPlot","GLOBAL",10)~ THEN BEGIN StillBrewing
+  SAY ~Still working, my dear <MANWOMAN>! Be patient for awhile longer.~
+  IF ~~ THEN EXIT
+END
+
+IF ~Global("IaSamaraPlot","GLOBAL",11)~ THEN BEGIN FinishedBrewing
+  SAY ~It is done! Here you go.~
+
+  IF ~Global("IaPotionType","LOCALS",1)~ THEN
+    REPLY ~Thank you, Reginald.~
+    DO ~GiveItemCreate("POTN06",Player1,8,0,0) GiveItemCreate("POTN07",Player1,4,0,0)~
+    GOTO Farewell
+
+  IF ~Global("IaPotionType","LOCALS",2)~ THEN
+    REPLY ~Thank you, Reginald.~
+    DO ~GiveItemCreate("POTN55",Player1,9,0,0)~
+    GOTO Farewell
+
+  IF ~Global("IaPotionType","LOCALS",3)~ THEN
+    REPLY ~Thank you, Reginald.~
     DO ~GiveItemCreate("POTN37",Player1,15,0,0)~
     GOTO Farewell
 END
 
 IF ~~ THEN BEGIN Farewell
-  SAY ~Here yo go. Now, I must take my leave. Farewell!~
-  IF ~~ THEN DO ~EscapeArea()~ EXIT
+  SAY ~My pleasure! Now, I must take my leave. Farewell!~
+  IF ~~ THEN
+    DO ~AddXPObject(Player1,15000)
+        AddXPObject(Player2,15000)
+        AddXPObject(Player3,15000)
+        AddXPObject(Player4,15000)
+        AddXPObject(Player5,15000)
+        AddXPObject(Player6,15000)
+        EscapeArea()~
+    EXIT
 END
