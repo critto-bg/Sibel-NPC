3cf9ea74b83c5cb63bfcd6786b26f03c445cad0d
RD <el.critto@gmail.com>
2024-01-15 19:17:21 +0400

    more fixes to EDE leather golem helper

--- SibelNPC/resources/dialogues/leat5.d
+++ SibelNPC/resources/dialogues/leat5.d
@@ -1,7 +1,7 @@
 BEGIN ~S!SLEAT5~
 
 IF ~NumTimesTalkedTo(0)~ THEN BEGIN Start
-  SAY ~Greetings, <CHARNAME>! Master sends his regrets that he may not stand by your side for reasons beyond his control.~
+  SAY ~Greetings, Sibel! Master sends his regrets that he may not stand by your side for reasons beyond his control.~
   = ~It is my Master's wish that you accept my aid in battle against the Rakshasa deity.~
 
   IF ~~ THEN EXIT
